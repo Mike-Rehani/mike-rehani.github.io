d9898827cdc5a84cddd27b6ac75d9f2a73a98e85
mrehani <mrehani73@gmail.com>
2024-12-28 02:46:40 -0600

    Fix MIME type issues and redeploy

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mike-rehani.github.io/'
+  base: '/'
 })
